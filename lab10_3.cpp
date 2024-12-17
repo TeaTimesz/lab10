1dd7f22a2485ca8ee5ad8d9deaf74e3b765a48bf
TeaTimesz <rawiphatmak@gmail.com>
2024-12-17 16:23:38 +0700

    completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+	source.open("score.txt");
+    string textline;
+    int i = 0;
+    float sum = 0;
+    float powsum = 0;
+    while(getline(source,textline)){
+        sum += stof(textline);
+        i++;
+    }
+    /*cout << "Number of data = "  << i << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << (1.0/(i + 1)) * sum << endl;
+    cout << "Standard deviation = " << sqrt(((1.0/(i + 1))) * sum - ((1.0/(i + 1)) * sum));*/
+    cout << i;
+}
